8a1c97fd6c15606ad4a6a9e97f5dc6495cd8c1af
Kirjia <enzo2101200110@gmail.com>
2026-05-19 11:02:35 +0200

    Refactor utils module: Clean up imports and enhance path handling in models.py

--- src/models/quantum/quantumMap.py
+++ src/models/quantum/quantumMap.py
@@ -1,12 +1,24 @@
+import multiprocessing
+
 from qiskit import QuantumCircuit
 import torch
-import torch.nn.functional as F
 from qiskit.circuit.library import real_amplitudes
-from qiskit.circuit import QuantumCircuit, ParameterVector
+from qiskit.circuit import Parameter, QuantumCircuit, ParameterVector
 import torch.nn as nn
+from qiskit_aer import AerSimulator
+from qiskit_aer.primitives import EstimatorV2 as EstimatorAer
+from qiskit_algorithms.gradients import ParamShiftEstimatorGradient
+from qiskit_machine_learning.neural_networks import EstimatorQNN
 from qiskit_machine_learning.connectors import TorchConnector
-from qiskit.primitives import BaseEstimatorV2 as BaseEstimator
-from django.db import models
+from qiskit_aer.primitives import EstimatorV2 as AerEstimator
+
+import os
+
+os.environ['OMP_NUM_THREADS'] = '12'
+os.environ['MKL_NUM_THREADS'] = '12'
+os.environ['OPENBLAS_NUM_THREADS'] = '12'
+
+
 
 def zero_padding(n_qubits, n_dim):
     resto = n_dim % n_qubits
@@ -22,55 +34,67 @@ def zero_padding(n_qubits, n_dim):
 
 
 
-class QuantumAnsats():
-    def __init__(self, n_qubits, n_dim):
-        self.n_qubits = n_qubits
-        self.n_dim = n_dim
-        self.d_padded = None
-        self.x = None
-        self.create_quantum_circuit(n_qubits, n_dim)
+def quantumAnsatz(n_qubits, n_dim) -> tuple[QuantumCircuit, int, list, list]:
+    d_padded = zero_padding(n_qubits, n_dim)
+    ansats = real_amplitudes(num_qubits=6, reps=2, name="Ansats")
 
-    def create_quantum_circuit(self,n_qubits, n_dim) -> tuple[QuantumCircuit, int]:
-        self.d_padded = zero_padding(n_qubits, n_dim)
-        ansats = real_amplitudes(num_qubits=6, reps=2, name="Ansats", entanglement="circular")
-        ansats.draw("mpl")
+    fm = QuantumCircuit(n_qubits)
+    input_params = ParameterVector("x", length=d_padded)
+    for i in range(d_padded):
+        fm.ry(input_params[i], i % n_qubits)  # Applica la rotazione Ry al qubit i%n_qubits
 
-        fm = QuantumCircuit(n_qubits)
-        self.x = ParameterVector("x", length=self.d_padded)
-        for i in range(self.d_padded):
-            fm.ry(self.x[i], i % n_qubits)  # Applica la rotazione Ry al qubit i%n_qubits
-
-        self.circ = fm.compose(ansats)
+    ansatz = fm.compose(ansats)
+    weight_params = [param for param in ansatz.parameters if param.name.startswith("θ")]
     
-    @property
-    def parameters(self):
-        return self.x
     
-    @property
-    def circuit(self):
-        return self.circ
-    
-    @property
-    def padded_dimension(self):
-        return self.d_padded
-    
-    @property
-    def full_parameters(self):
-        return self.circ.parameters
+    return ansatz, d_padded, input_params, weight_params
+       
 
 
 
 class VQC(nn.Module):
-    def __init__(self, n_qubits, n_dim, estimator :BaseEstimator):
+    def __init__(self, n_qubits, n_dim, obs=None):
         super(VQC, self).__init__()
-        self.quantum_ansats = QuantumAnsats(n_qubits, n_dim)
-        initial_weights = torch.empty(estimator.num_weights).uniform_(-0.01, 0.01)
-        self.quantum_layer = TorchConnector(estimator, initial_weights=initial_weights)
-        self.linear = nn.Linear(n_qubits, 4)  # 4 classi di output
+        self.n_qubits = n_qubits
+        self.quantum_ansats, self.d_padded, self.input_params, self.weight_params = quantumAnsatz(n_qubits, n_dim)
+
+        #estimetor = StatevectorEstimator()
+
+        estimator = AerEstimator()
+        estimator.options.simulator = {
+            "method": "statevector",  # Simulazione esatta richiesta dal prof
+            "device": "GPU",          # Usa la scheda video
+            "cuStateVec_enable": True # Attiva i driver quantistici NVIDIA ad alte prestazioni
+        }
         
+        
+
+        self.q_weights = nn.Parameter(torch.empty(len(list(self.quantum_ansats.parameters))).uniform_(-0.01, 0.01))
+        
+        gradients = ParamShiftEstimatorGradient(estimator)
+
+        # Create the Estimator QNN
+        self.qnn = EstimatorQNN(circuit=self.quantum_ansats,
+                                observables=obs,
+                                input_params=self.input_params,
+                                weight_params=self.weight_params,
+                                estimator=estimator,
+                                gradient=gradients,
+                                input_gradients=True)
+
+        # Connect to PyTorch
+        self.quantum_layer = TorchConnector(self.qnn)
+
+        # Classical output layer
+        num_observables = len(obs) if obs else n_qubits
+        self.linear = nn.Linear(num_observables, 4)
+
     def forward(self, x):
-        quantum_output = self.quantum_layer(x)
-        output = self.linear(quantum_output)
-        return output
+        q_out = self.quantum_layer(x)
+
+        
+        # 5. Classificazione finale
+        logits = self.linear(q_out)
+        return logits
     
     
